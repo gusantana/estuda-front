2a64c7003517f068fa6cbbb71150fcb8a389eeaa
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-27 15:22:05 -0400

    Adicionado menu de cards na tela inicial

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 // import { Container } from './styles';
+// import './index.css';
 
 const Home: React.FC = () => {
   return (
     <div className="container">
-      <h1>Página Inicial</h1>
+      <div className="d-flex" style={{gap: '1rem'}}>
+        <Link to="/Escola/" className="card">
+          <div className="card-body">Escolas</div>
+          <div className="card-footer">Escolas</div>
+        </Link>
+
+        <Link to="/Aluno/" className="card">
+          <div className="card-body">Alunos</div>
+          <div className="card-footer">Alunos</div>
+        </Link>
+
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
