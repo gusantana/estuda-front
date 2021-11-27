e111b0d522e4761e3f776005d148d60b8ebb241d
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-27 15:17:51 -0400

    Adicionado componente de Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,34 @@
+import { useField } from "@unform/core";
+import React, { RefObject, useEffect, useRef } from "react";
+
+interface Props {
+  name: string;
+}
+
+type InputProps = JSX.IntrinsicElements["input"] & Props;
+
+const Input: React.FC<InputProps> = ({ name, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const { fieldName, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef,
+      getValue: (ref) => {
+        return ref.current?.value;
+      },
+      setValue: (ref, value) => {
+        ref.current.value = value;
+      },
+      clearValue: (ref) => {
+        ref.current.value = "";
+      },
+    });
+  }, [fieldName, registerField]);
+
+  return <input name={fieldName} {...rest} id={fieldName} ref={inputRef}/>;
+};
+
+export default Input;
