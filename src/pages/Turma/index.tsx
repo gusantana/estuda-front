0d8a7435d39e80ecd44ff0f9cb08942dbdfc126b
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-28 14:25:47 -0400

    buscando lista de turmas

--- src/pages/Turma/index.tsx
+++ src/pages/Turma/index.tsx
@@ -5,13 +5,17 @@ import { Link } from "react-router-dom";
 
 import api from "../api";
 // import { Container } from './styles';
-import { AxiosResponse } from 'axios';
+import { AxiosResponse } from "axios";
 import Select from "../../components/Select";
+import { Props } from "react-select/dist/declarations/src/Select";
 
 interface TurmaProps {
   id: string;
-  nome: string;
-  endereco: string;
+  id_escola: string;
+  nivel_ensino: string;
+  serie: string;
+  turno: string;
+  ano: string;
 }
 
 interface EscolaProps {
@@ -21,22 +25,33 @@ interface EscolaProps {
 
 const Turma: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-  
+
   const [turmas, setTurmas] = useState<TurmaProps[]>([]);
-  const [escolas, setEscolas] = useState<EscolaProps[]>([]);
+  const [escolas, setEscolas] = useState<any>([]);
 
   const buscaTumasDeEscola: SubmitHandler<any> = (data) => {
-    // api.get("/Turma/getPorEscola/").then(retornoBuscaEscolas);
-    console.log(data);
+    // console.log(data);
+    api
+      .get("/Turma/getPorEscola/", { params: data })
+      .then(retornoBuscaTumasDeEscola);
+  };
+
+  const retornoBuscaTumasDeEscola = (resposta: AxiosResponse<any>) => {
+    // console.log(retorno);
+    setTurmas(resposta.data);
   };
 
   const buscaEscolas = () => {
-    api.get('/Escola').then(retornoBuscaEscolas);
-  }
+    api.get("/Escola").then(retornoBuscaEscolas);
+  };
 
-  const retornoBuscaEscolas = (resposta: AxiosResponse<any>) => {
-    setEscolas(resposta.data);
-  }
+  const retornoBuscaEscolas = (resposta: AxiosResponse<EscolaProps[]>) => {
+    const opcoesEscolas = resposta.data.map((escola) => ({
+      value: escola.id,
+      label: escola.nome,
+    }));
+    setEscolas(opcoesEscolas);
+  };
 
   const inicio = () => {
     buscaEscolas();
@@ -51,8 +66,8 @@ const Turma: React.FC = () => {
           <Link to="/Home/" className="btn btn-outline-secondary">
             Voltar
           </Link>
-          <Link to="/Escola/adicionar" className="btn btn-primary ms-3">
-            Nova Escola
+          <Link to="/Turma/adicionar" className="btn btn-primary ms-3">
+            Nova Turma
           </Link>
         </div>
       </div>
@@ -60,47 +75,46 @@ const Turma: React.FC = () => {
       <div className="card">
         <h6 className="card-header">Turmas</h6>
         <div className="card-body">
-          <Form ref={formRef} onSubmit={buscaTumasDeEscola} >
+          <Form ref={formRef} onSubmit={buscaTumasDeEscola}>
             <div className="mb-3">
-              <label htmlFor="escola" className="form-label">Escola</label>
-              <Select name="id_escola" id="escola" className="form-select">
-                <option value="">Selecione uma escola</option>
-                {escolas.map(escola => (
-                  <option value={escola.id} key={escola.id}>{escola.nome}</option>
-                ))}
-              </Select>
+              <label htmlFor="escola" className="form-label">
+                Escola
+              </label>
+              <Select name="id_escola" id="escola" options={escolas} />
             </div>
 
             <div>
-              <button type="submit" className="btn btn-info text-light">Buscar</button>
+              <button type="submit" className="btn btn-info text-light">
+                Buscar
+              </button>
             </div>
           </Form>
         </div>
-        <div className="table-responsive">
-          <table className="table table-striped table-hover mb-0">
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th className="text-center">Ações</th>
+        <table className="table table-striped table-hover mb-0">
+          <thead>
+            <tr>
+              <th>Série</th>
+              <th>Nível</th>
+              <th className="text-center">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {turmas.map((turma: TurmaProps) => (
+              <tr key={turma.id}>
+                <td>{turma.serie}</td>
+                <td>{turma.nivel_ensino}</td>
+                <td className="text-center">
+                  <Link
+                    to={`/Turma/editar/${turma.id}`}
+                    className="btn btn-secondary btn-sm"
+                  >
+                    Editar
+                  </Link>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {turmas.map((turma: TurmaProps) => (
-                <tr key={turma.id}>
-                  <td>{turma.nome}</td>
-                  <td className="text-center">
-                    <Link
-                      to={`/Escola/editar/${turma.id}`}
-                      className="btn btn-secondary btn-sm"
-                    >
-                      Editar
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
