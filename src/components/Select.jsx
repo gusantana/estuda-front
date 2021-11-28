e52ce745052e6a4b14fb658fad98ff3c1d2363a4
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-28 16:29:26 -0400

    Removido o uso do react-select

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,38 +1,32 @@
-import ReactSelect from "react-select";
-import { useField } from "@unform/core";import { useEffect, useRef } from "react";
-;
+import { useField } from "@unform/core";
+import { useEffect, useRef } from "react";
 
-const Select = ({ name, ...rest }) => {
+const Select = ({ name, children, ...rest }) => {
   const selectRef = useRef(null);
-  const { fieldName, defaultValue, registerField, clearError } =
-    useField(name);
+  const { fieldName, defaultValue, registerField, clearError } = useField(name);
 
   useEffect(() => {
     registerField({
       name: fieldName,
       ref: selectRef,
-      getValue: ref => {
-        if (ref.current.getValue()[0]) {
-          return ref.current.getValue()[0].value;
-        } 
-        return null;
+      getValue: (ref) => {
+        return ref.current.value;
       },
-      setValue: (ref, valor) => {
-        ref.current.defaultValue = valor;
-      }
+      setValue: (ref, value) => {
+        ref.current.value = value;
+      },
     });
   }, [fieldName, registerField]);
 
   return (
-    <ReactSelect
-      defaultValue={defaultValue}
-      ref={selectRef}
-      classNamePrefix="react-select"
-      placeholder="Selecione"
-      onFocus={clearError}
-      {...rest}
-      isMulti={false}
-    />
+    <select name={name} {...rest} ref={selectRef}>
+      {children}
+      {/* {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.text}
+        </option>
+      ))} */}
+    </select>
   );
 };
 
