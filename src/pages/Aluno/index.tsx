dec378bb35712c2d01df9393061483eda68f9fb1
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-28 17:01:19 -0400

    Adicionado tela de listagem de alunos

--- src/pages/Aluno/index.tsx
+++ src/pages/Aluno/index.tsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import { FormHandles, SubmitHandler } from "@unform/core";
+import { Form } from "@unform/web";
+import { AxiosResponse } from "axios";
+import React, { useEffect, useRef, useState } from "react";
 
-// import { Container } from './styles';
+import { Link } from "react-router-dom";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
+import api from "../api";
 
-const Aluno: React.FC = () => {
-  return <div className="container">
-      <h1>Página do Aluno</h1>
-  </div>;
+interface EscolaProps {
+  id: string;
+  nome: string;
 }
+
+const Aluno: React.FC = ({ props }: any) => {
+  const formRef = useRef<FormHandles>(null);
+
+  const [alunos, setAlunos] = useState<any>([]);
+
+  const inicio = () => {
+    buscaAlunos();
+  };
+
+  const buscaAlunos = () => {
+    api.get("/Aluno").then(retornoBuscaAlunos);
+  };
+
+  const retornoBuscaAlunos = (resposta: AxiosResponse<any[]>) => {
+    setAlunos(resposta.data);
+  };
+
+  const submeter: SubmitHandler<any> = (data) => {
+    console.log(data);
+    api.post("/Aluno/salvar", data).then(retornoSubmeter);
+  };
+
+  const retornoSubmeter = (resposta: any) => {
+    formRef.current?.setData(resposta.data);
+  };
+
+  useEffect(inicio, []);
+
+  return (
+    <>
+      <div className="row mb-3">
+        <div className="col-auto">
+          <Link to="/Home/" className="btn btn-outline-secondary">
+            Voltar
+          </Link>
+        </div>
+      </div>
+
+      <div className="card">
+        <h6 className="card-header">Alunos</h6>
+        <table className="table table-striped table-hover mb-0">
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>E-mail</th>
+              <th className="text-center">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {alunos.map((aluno: any) => (
+              <tr key={aluno.id}>
+                <td>{aluno.nome}</td>
+                <td>{aluno.email}</td>
+                <td className="text-center">
+                  <Link
+                    to={`/Aluno/editar/${aluno.id}`}
+                    className="btn btn-secondary btn-sm"
+                  >
+                    Editar
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
 export default Aluno;
