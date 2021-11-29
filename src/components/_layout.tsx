61d9a1e67bfbc03cbe99695d820dbb883f9bd784
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-29 18:33:35 -0400

    Decorado layout das páginas

--- src/components/_layout.tsx
+++ src/components/_layout.tsx
@@ -4,11 +4,16 @@ import React from "react";
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <div className="container">
-      <div>cabeçalho</div>
-      {children}
-      <div id="footer">
-          footer
+    <div className="container d-flex flex-column">
+      <div className=" py-4 alert">
+        <h4>Escola.com</h4>
+      </div>
+      <div className="f-1 py-5">{children}</div>
+
+      <div id="footer" className="py-4">
+        <div className="text-center">
+          <small>Escola.com - 2021 - 2021</small>
+        </div>
       </div>
     </div>
   );
