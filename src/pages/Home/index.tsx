43d7fc932e58bc40cc7c111332690359fa285d3e
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-29 18:36:07 -0400

    Utilizando as imagens nos cards da tela inicial

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ImagemAluno } from "../../components/images/aluno";
+import { ImagemEscola } from "../../components/images/escola";
+import { ImagemTurma } from "../../components/images/turma";
+
 
 // import { Container } from './styles';
 // import './index.css';
@@ -7,22 +11,27 @@ import { Link } from "react-router-dom";
 const Home: React.FC = () => {
   return (
     <div className="container">
-      <div className="d-flex" style={{gap: '1rem'}}>
-        <Link to="/Escola/" className="card">
-          <div className="card-body">Escolas</div>
-          <div className="card-footer">Escolas</div>
+      <div className="d-flex" style={{ gap: "1rem" }}>
+        <Link to="/Escola/" className="card card-principal">
+          <div className="card-body p-4 ">
+            <img src={ImagemEscola} alt="Escola" className="imagem-card" />
+          </div>
+          <div className="card-footer text-center">Escolas</div>
         </Link>
 
-        <Link to="/Aluno/" className="card">
-          <div className="card-body">Alunos</div>
-          <div className="card-footer">Alunos</div>
+        <Link to="/Aluno/" className="card card-principal">
+          <div className="card-body">
+            <img src={ImagemAluno} alt="Escola" className="imagem-card" />
+          </div>
+          <div className="card-footer text-center">Alunos</div>
         </Link>
 
-        <Link to="/Turma/" className="card">
-          <div className="card-body">Turmas</div>
-          <div className="card-footer">Turmas</div>
+        <Link to="/Turma/" className="card card-principal">
+          <div className="card-body">
+            <img src={ImagemTurma} alt="Escola" className="imagem-card" />
+          </div>
+          <div className="card-footer text-center">Turmas</div>
         </Link>
-
       </div>
     </div>
   );
